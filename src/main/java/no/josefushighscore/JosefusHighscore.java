f87b792292f480b88916591f3d0b25b3981d3e25
Tofik Sahraoui <tofiksa@gmail.com>
2022-06-06 10:21:22 +0200

    [ADD] - Spring boot framework       - application properties files

---
+++ src/main/java/no/josefushighscore/JosefusHighscore.java
@@ -0,0 +1,13 @@
+package no.josefushighscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosefusHighscore {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JosefusHighscore.class, args);
+    }
+
+}
