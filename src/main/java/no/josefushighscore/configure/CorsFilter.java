bae9837414044e51bea63e48c2623b1cde55ab0e
Tofik Sahraoui <tofiksa@gmail.com>
2022-06-11 20:29:18 +0200

    [ADD] Sikring av endepunkter og CORS filter

---
+++ src/main/java/no/josefushighscore/configure/CorsFilter.java
@@ -0,0 +1,47 @@
+package no.josefushighscore.configure;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+public class CorsFilter implements Filter {
+
+    private final Logger LOG = LoggerFactory.getLogger(CorsFilter.class);
+
+    public CorsFilter() {
+        LOG.debug("CORSFILTER INIT");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        response.setHeader("Accept","application/json, text/plain, */*");
+        chain.doFilter(request, res);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
---
+++ src/main/java/no/josefushighscore/configure/SecurityConfig.java
@@ -0,0 +1,30 @@
+package no.josefushighscore.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .httpBasic().disable()
+                .csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .antMatchers("/register/**").permitAll()
+                .antMatchers("/auth/**").permitAll().anyRequest().authenticated();
+
+
+        return http.build();
+    }
+
+}
